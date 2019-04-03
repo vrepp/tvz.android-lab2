c5df5da889fd0920e555c1e0921e8eefa568e3b6
Valentin Rep <valentin.rep@gmail.com>
2019-04-03 09:36:54 +0200

    Initial commit

---
+++ app/src/main/java/hr/tvz/android/listarep/MainActivity.kt
@@ -0,0 +1,12 @@
+package hr.tvz.android.listarep
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
